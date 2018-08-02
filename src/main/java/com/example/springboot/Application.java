7a33bbee566058b5339cb6e117f817b9f6b64f30
a_matsumoto <a_matsumoto@dialonet.co.jp>
2018-08-02 13:56:35 +0900

    Applicationクラス作成

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,18 +1,36 @@
 package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+/**
+ * 
+ * 
+ * 
+ * @author a_matsumoto
+ *
+ */
+@SpringBootApplication
 
 public class Application {
+	private static ConfigurableApplicationContext context = null;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	public static <T> T getBean(Class<T> clazz) {
+		return getContext().getBean(clazz);
+	}
+
+	private static ConfigurableApplicationContext getContext() {
+		if (context == null) {
+			String args[] = new String[1];
+			args[0] = "dummy";
+			context = SpringApplication.run(Application.class, args);
+		}
+		return context;
 	}
 
 }
